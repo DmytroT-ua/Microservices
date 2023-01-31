ebe96b59076aeda2d1257b843a7b9cefd38727a6
dtarasiuta <dtarasiuta@b2bsoft.com>
2023-01-31 22:54:59 +0200

    tests for customer api

---
+++ CustomerApi/Tests/CustomerApi.Data.Tests/DatabaseTestBase.cs
@@ -0,0 +1,28 @@
+﻿using CustomerApi.Data.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerApi.Data.Tests
+{
+    public class DatabaseTestBase : IDisposable
+    {
+        protected readonly DatabaseContext Context;
+
+        public DatabaseTestBase()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            Context = new DatabaseContext(options);
+
+            Context.Database.EnsureCreated();
+
+            DatabaseInitializer.Initialize(Context);
+        }
+
+        public void Dispose()
+        {
+            Context.Database.EnsureDeleted();
+
+            Context.Dispose();
+        }
+    }
+}
